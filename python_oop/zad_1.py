af4818c023bb14e6964af1961e1e5ccf06e76509
Hanna Makurat-Snuzik <hanna.makurat@gmail.com>
2025-12-28 16:08:51 +0100

    Python OOP - Commit

---
+++ python_oop/zad_1.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self):
+        if len(self.marks) == 0:
+            return False
+        avg = sum(self.marks) / len(self.marks)
+        return avg > 50
+
+student1 = Student("Agata Zygiert", [65, 76, 89])
+student2 = Student("Adam Zyguś", [37, 28, 47])
+
+print(student1.is_passed())
+print(student2.is_passed())
